db25a725dedb4b85663d28abefc916fc6a0704ae
Carlos Zamora <czamora5@ucol.mx>
2022-09-30 04:47:10 -0500

    feat: include Home page using the Login and HeroImage components

---
+++ src/routes/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import HeroImage from '../../components/HeroImage/HeroImage';
+import Login from '../../components/Login/Login';
+
+export default function Home() {
+  return (
+    <HeroImage>
+      <Login />
+    </HeroImage>
+  )
+}
