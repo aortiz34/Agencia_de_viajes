e476eff66c082f15d194db35c2f3df58cad89b13
Carlos Zamora <czamora5@ucol.mx>
2022-09-30 15:48:36 -0500

    feat: implement styles in the destinos page

--- src/components/destino-card/destino-card.component.jsx
+++ src/components/destino-card/destino-card.component.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { DestinosContext } from '../../contexts/destinos.context';
 
+import './destino-card.styles.scss';
+import pencil from '../../assets/pencil.svg';
+import trashCan from '../../assets/trash-can.svg';
+
 const DestinoCard = ({ destino }) => {
 
     const { nombreDelDestino, idDestino, codigoPostal } = destino;
@@ -10,16 +14,21 @@ const DestinoCard = ({ destino }) => {
 
     return (
         <div className="destino-card-container">
-            <samp>{nombreDelDestino}</samp><br />
-            <samp>{idDestino}</samp><br />
-            <samp>{codigoPostal}</samp><br />
-            <Button>
-                <Link to={`editar-destino/${idDestino}`}>Editar</Link>
-            </Button>
-            <Button onClick={() => destinoEliminado(destino)}>
-                Eliminar
-            </Button>
-            <br />
+            <div className="destino-card-data">
+                <span className="destino-card-id">{idDestino}</span>
+                <span className="destino-card-name">{nombreDelDestino}</span>
+                <span className="destino-card-cp">{codigoPostal}</span>
+            </div>
+            <div className="destino-card-actions">
+                <Link to={`/destinos/editar-destino/${idDestino}`}>
+                    <Button className="destino-card-actions-button" tabIndex="-1">
+                        <img className="destino-card-actions-img" src={pencil} title="Editar destino" alt="Un lápiz"/>
+                    </Button>
+                </Link>
+                <Button className="destino-card-actions-button" onClick={() => destinoEliminado(destino)}>
+                    <img className="destino-card-actions-img" src={trashCan} title="Eliminar destino" alt="Un bote de basura"/>
+                </Button>
+            </div>
         </div>
     );
 }
--- src/routes/destinos/destinos.component.jsx
+++ src/routes/destinos/destinos.component.jsx
@@ -4,22 +4,32 @@ import { Link } from "react-router-dom";
 
 import Button from "../../components/button/button.component";
 import DestinoCard from "../../components/destino-card/destino-card.component";
+import './destinos.styles.scss';
 
 const Destinos = () => {
 
     const { destinos } = useContext(DestinosContext);
 
     return (
-        <div>
-            <Link to="agregar-destino">
-                <Button>
+        <div className="destinos-catalogue-wrapper container">
+            <Link className="destinos-catalogue-link" to="/destinos/agregar-destino">
+                <Button tabIndex="-1">
                     Agregar nuevo Destino
                 </Button>
             </Link>
-            <div className="destinos-catalogue-container">
-                {destinos.map((destino) => (
-                    <DestinoCard key={destino.idDestino} destino={destino} />
-                ))}
+            <div className="destinos-catalogue">
+                <header className="destinos-catalogue-header-container">
+                    <div className="destinos-catalogue-header">
+                        <span className="destinos-catalogue-id">ID</span>
+                        <span className="destinos-catalogue-name">Nombre</span>
+                        <span className="destinos-catalogue-cp">Código postal</span>
+                    </div>
+                </header>
+                <div className="destinos-catalogue-container">
+                    {destinos.map((destino) => (
+                        <DestinoCard key={destino.idDestino} destino={destino} />
+                    ))}
+                </div>
             </div>
         </div>
     );
